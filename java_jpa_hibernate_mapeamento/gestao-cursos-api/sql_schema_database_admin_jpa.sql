94b58f9540a47f348bdca673eb93c871a838d040
LBarros360 <lbarros360@gmail.com>
2025-11-07 22:40:24 -0300

    Add files via upload

---
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/sql_schema_database_admin_jpa.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Aluno (id bigint not null, email varchar(255), matricula varchar(255), nascimento datetime, nome varchar(255), primary key (id))
+CREATE TABLE Curso (id bigint not null, nome varchar(255), sigla varchar(255), materialCurso_Id bigint, professor_id bigint, primary key (id))
+CREATE TABLE Curso_Aluno (cursos_id bigint not null, alunos_id bigint not null)
+CREATE TABLE Endereco (id bigint not null, bairro varchar(255), cep integer, cidade varchar(255), estado varchar(255), logradouro varchar(255), numero varchar(255), aluno_id bigint, primary key (id))
+CREATE TABLE hibernate_sequence (next_val bigint)
+CREATE TABLE MaterialCurso (Id bigint not null, url varchar(255), primary key (Id))
+CREATE TABLE Professor (id bigint not null, email varchar(255), matricula varchar(255), nome varchar(255), primary key (id))
+CREATE TABLE Telefone (id bigint not null, ddd varchar(255), numero varchar(255), aluno_id bigint, primary key (id))
